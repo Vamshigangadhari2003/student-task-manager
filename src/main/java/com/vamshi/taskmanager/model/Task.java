73beb91b1367ce4a51827bcf53bc4b5da261a73f
vamshi Gangadhari <vamshigangadhari2003@gmail.com>
2026-06-27 17:21:33 +0530

    Day 1/task2: created Task entity with JPA annotations and MySQL config

---
+++ src/main/java/com/vamshi/taskmanager/model/Task.java
@@ -0,0 +1,33 @@
+package com.vamshi.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private String priority;
+
+    @Column(nullable = false)
+    private String status;
+
+    private LocalDate dueDate;
+
+}
